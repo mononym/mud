3b571f92fd953781b93623fd9f48b8f33b26aae6
Chris <mononym@users.noreply.github.com>
2020-09-22 01:45:45 +0200

    basic grid setup

--- client/src/pages/GameClientPage.vue
+++ client/src/pages/GameClientPage.vue
@@ -1,14 +1,106 @@
 <template>
-  <q-page class="flex col flex-center">
-    testing
+  <q-page class="col no-scroll game-client-page">
+    <grid-layout
+      :layout.sync="layout"
+      :col-num="numColumns"
+      :max-rows="maxNumRows"
+      :row-height="rowHeight"
+      :is-draggable="true"
+      :is-resizable="true"
+      :is-mirrored="false"
+      :vertical-compact="true"
+      :margin="marginString"
+      :use-css-transforms="true"
+      :parent="true"
+      :lock-aspect-ratio="true"
+      id="gameClientLayout"
+      class="fit"
+    >
+      <grid-item
+        v-for="item in layout"
+        :x="item.x"
+        :y="item.y"
+        :w="item.w"
+        :h="item.h"
+        :i="item.i"
+        :key="item.i"
+        :static="false"
+        style="background-color: blue"
+        class="game-client-window"
+      >
+        {{ item.i }}
+      </grid-item>
+    </grid-layout>
   </q-page>
 </template>
 
-<style></style>
+<style lang="sass">
+.vue-grid-item {background-color: blue}
+main.q-page.game-client-page {max-height: calc(100vh - 50px)}
+.game-client-window {max-height: calc(100% - 20px)}
+</style>
 
 <script lang="ts">
+import VueGridLayout from 'vue-grid-layout';
+
+interface LayoutInterface {
+  x: number;
+  y: number;
+  w: number;
+  h: number;
+  i: string;
+}
+
+interface WindowInterface {
+  key: string;
+  title: string;
+  layout: LayoutInterface;
+}
+
 export default {
   name: 'LandingPage',
-  methods: {}
+  components: { VueGridLayout },
+  data(): {
+    marginX: number;
+    marginY: number;
+    numColumns: number;
+    maxNumRows: number;
+    rowHeight: number;
+    windows: WindowInterface[];
+  } {
+    return {
+      marginX: 10,
+      marginY: 10,
+      numColumns: 12,
+      maxNumRows: 12,
+      rowHeight: 150,
+      windows: [
+        {
+          key: 'story',
+          title: 'Story',
+          layout: { x: 0, y: 0, w: 12, h: 12, i: 'story' }
+        }
+      ]
+    };
+  },
+  computed: {
+    layout: function(): LayoutInterface[] {
+      return this.windows.map(map => map.layout);
+    },
+    marginString: function(): number[] {
+      return [this.marginX, this.marginY];
+    }
+  },
+  mounted() {
+    this.calculateRowHeight();
+  },
+  methods: {
+    calculateRowHeight() {
+      console.log('here');
+      const el = document.getElementById('gameClientLayout');
+      this.rowHeight =
+        el !== null ? Math.floor(el.clientHeight / this.maxNumRows) : 0;
+    }
+  }
 };
 </script>
