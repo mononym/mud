a676f6494215aa28a2a74d6900247522af4c3adb
Chris <mononym@users.noreply.github.com>
2021-03-03 06:04:25 +0100

    try different method of getting env variables

--- server/CodeDeploySetupEnvironmentVariables.sh
+++ server/CodeDeploySetupEnvironmentVariables.sh
@@ -1,35 +1,14 @@
 #!/usr/bin/env bash
 
-# install pip
-OS=`cat /etc/os-release | grep '^NAME=' |  tr -d \" | sed 's/\n//g' | sed 's/NAME=//g'`
-
-if [ "$OS" == "Ubuntu" ]; then
-    apt-get -y update
-    apt-get -y install python-pip
-elif [ "$OS" == "Amazon Linux AMI" ]; then
-    yum update -y 
-    yum install -y python-pip
-fi
-
-# install aws-cli
-pip install --upgrade pip &> /dev/null
-pip install awscli --ignore-installed six &> /dev/null
-
-# add boot script which loads environment variables
-# cat > /etc/profile.d/export_instance_tags.sh << 'EOF'
-    # fetch instance info
+getInstanceTags () {
     INSTANCE_ID=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
-    REGION=${AWS::Region}
+    EC2_AVAIL_ZONE=`curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone`
+    EC2_REGION="`echo \"$EC2_AVAIL_ZONE\" | sed -e 's:\([0-9][0-9]*\)[a-z]*\$:\\1:'`"
 
-    # export instance tags
-    export_statement=$(aws ec2 describe-tags --region "$REGION" \
-                            --filters "Name=resource-id,Values=$INSTANCE_ID" \
-                            --query 'Tags[?!contains(Key, `:`)].[Key,Value]' \
-                            --output text | \
-                            sed -E 's/^([^\s\t]+)[\s\t]+([^\n]+)$/export \1="\2"/g')
-    eval $export_statement
+    # Print tags as 'export TAG_Key="Value"'
+    aws ec2 describe-tags --region $EC2_REGION --filters "Name=resource-id,Values=$INSTANCE_ID" | \
+      python -c "import sys,json; map(lambda t: sys.stdout.write('export TAG_%s=\"%s\"\n' % (t['Key'], t['Value'])), json.load(sys.stdin)['Tags'])"
+}
 
-    # export instance info
-    export INSTANCE_ID
-    export REGION
-# EOF
+# Export variables from tags
+$(getInstanceTags)
