8207965f9396e0f4d0f99726dc606e869ed53ba7
Chris <mononym@users.noreply.github.com>
2021-03-04 02:41:37 +0100

    change sit callback

--- server/lib/mud/engine/command/callbacks/sit.ex
+++ server/lib/mud/engine/command/callbacks/sit.ex
@@ -5,7 +5,7 @@ defmodule Mud.Engine.Command.Sit do
   If no target is provided, the Character will sit in place.
 
   Syntax:
-    - sit < on | in > target
+    - sit [on] target
 
   Examples:
     - sit
@@ -23,169 +23,56 @@ defmodule Mud.Engine.Command.Sit do
 
   require Logger
 
-  @spec build_ast([Mud.Engine.Command.AstNode.t(), ...]) ::
-          Mud.Engine.Command.AstNode.OneThing.t()
-  def build_ast(ast_nodes) do
-    Mud.Engine.Command.AstUtil.build_one_thing_ast(ast_nodes)
+  def build_ast(_ast_nodes) do
+    :ok
   end
 
   @impl true
   def execute(%Context{} = context) do
-    ast = context.command.ast
-
-    if is_nil(ast.thing) do
-      make_character_sit(context)
+    if context.character.status.position == "sitting" do
+      Context.append_message(
+        context,
+        Message.new_story_output(
+          context.character.id,
+          "You are already sitting.",
+          "system_info"
+        )
+      )
     else
-      sit_on_target(context)
-    end
-  end
+      updated_status =
+        Mud.Engine.Character.Status.update!(context.character.status, %{position: "sitting"})
 
-  def sit_on_target(context) do
-    ast = context.command.ast
+      character = Map.put(context.character, :status, updated_status)
 
-    if Util.is_uuid4(ast.thing.input) do
-      item = Item.get!(ast.thing.input)
-
-      if item.area_id == context.character.area_id do
-        make_character_sit(context, item)
-      else
-        Context.append_output(
-          context,
+      others =
+        Character.list_others_active_in_areas(
           context.character.id,
-          "{{error}}I'm sorry #{context.character.name}, I'm afraid I can't do that.{{/error}}",
-          "error"
-        )
-      end
-    else
-      result =
-        Search.find_matches_in_area_v2(
-          [:item],
-          context.character.area_id,
-          ast.thing.input,
-          ast.thing.which
+          context.character.area_id
         )
 
-      case result do
-        {:ok, [thing]} ->
-          make_character_sit(context, thing.match)
+      other_msg =
+        others
+        |> Message.new_story_output()
+        |> Message.append_text("[#{context.character.name}]", "character")
+        |> Message.append_text(" sits down.", "base")
 
-        {:ok, _matches} ->
-          Context.append_output(
-            context,
-            context.character.id,
-            "Multiple potential places to sit found. Please be more specific.",
-            "error"
-          )
+      self_msg =
+        context.character.id
+        |> Message.new_story_output()
+        |> Message.append_text("You", "character")
+        |> Message.append_text(" sit down.", "base")
 
-        {:error, :no_match} ->
-          Context.append_output(
-            context,
-            context.character.id,
-            "Could not find anywhere to sit.",
-            "error"
-          )
-      end
-    end
-  end
-
-  @spec make_character_sit(context :: Context.t(), furniture_object :: Object.t() | nil) ::
-          Context.t()
-  defp make_character_sit(context, furniture_object \\ nil) do
-    char = context.character
-
-    cond do
-      char.position == Character.sitting() ->
-        Context.append_message(
-          context,
-          Message.new_output(
-            context.character.id,
-            "You are already sitting down!",
-            "error"
-          )
-        )
-
-      furniture_object == nil ->
-        update = %{
-          position: Character.sitting(),
-          relative_position: "",
-          relative_item_id: nil
-        }
-
-        {:ok, char} = Character.update(context.character, update)
-
-        others =
-          Character.list_others_active_in_areas(context.character.id, context.character.area_id)
-
-        context
-        |> Context.append_message(
-          Message.new_output(
-            others,
-            "#{context.character.name} sits down.",
-            "info"
-          )
-        )
-        |> Context.append_message(
-          Message.new_output(
-            context.character.id,
-            "You sit down.",
-            "info"
-          )
-        )
-        |> Context.append_event(
-          others,
-          UpdateArea.new(:update, char)
-        )
-        |> Context.append_event(
-          context.character.id,
-          UpdateCharacter.new(%{character: char})
-        )
-
-      furniture_object != nil and furniture_object.is_furniture and
-          char.position != Character.sitting() ->
-        update = %{
-          position: Character.sitting(),
-          relative_position: "on",
-          relative_item_id: furniture_object.id
-        }
-
-        Character.update(context.character, update)
-
-        others =
-          Character.list_others_active_in_areas(context.character.id, context.character.area_id)
-
-        context
-        |> Context.append_message(
-          Message.new_output(
-            others,
-            "#{context.character.name} sits down on #{furniture_object.description.short}.",
-            "info"
-          )
-        )
-        |> Context.append_message(
-          Message.new_output(
-            context.character.id,
-            "You sit down on #{furniture_object.description.short}.",
-            "info"
-          )
-        )
-        |> Context.append_event(
-          others,
-          UpdateArea.new(:update, char)
-        )
-        |> Context.append_event(
-          context.character.id,
-          UpdateCharacter.new(%{character: char})
-        )
-
-      true ->
-        Context.append_message(
-          context,
-          Message.new_output(
-            context.character.id,
-            "Unfortunately, #{furniture_object.description.short} can not be sat on.",
-            "error"
-          )
-        )
+      context
+      |> Context.append_event(
+        character.id,
+        UpdateCharacter.new(%{action: "status", status: updated_status})
+      )
+      |> Context.append_event(
+        [context.character_id | others],
+        UpdateArea.new(%{action: :update, other_characters: [character]})
+      )
+      |> Context.append_message(other_msg)
+      |> Context.append_message(self_msg)
     end
   end
 end
