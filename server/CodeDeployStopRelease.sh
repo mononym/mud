b7c0faa431e5c6dc7a7b1f73ba63e001a2bc37e1
Chris <mononym@users.noreply.github.com>
2021-03-04 03:00:00 +0100

    ignore errors stopping previous app

--- server/CodeDeployStopRelease.sh
+++ server/CodeDeployStopRelease.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
 export HOME=/home/ubuntu
-/home/ubuntu/apps/mud/bin/prod stop
+# Ignore error in case the server is not up, for example. Just assume this works or does not apply.
+/home/ubuntu/apps/mud/bin/prod stop || true
